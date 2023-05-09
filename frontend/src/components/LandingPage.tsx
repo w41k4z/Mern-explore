d09ddeedef27767c3dd9ad506a603a6b5d8d2633
w41k4z <alainricor@gmail.com>
2023-05-09 16:32:12 +0300

    Fixing routing

--- frontend/src/components/LandingPage.tsx
+++ frontend/src/components/LandingPage.tsx
@@ -1,11 +1,14 @@
-import '../assets/css/LandingPage.css';
+import { Link } from "react-router-dom";
+import "../assets/css/LandingPage.css";
 
 const LandingPage = () => {
-    return (
-        <div className='landingPage'>
-            
-        </div>
-    );
-}
+  return (
+    <div className="landingPage">
+      <Link to="/society">
+        <button>Go</button>
+      </Link>
+    </div>
+  );
+};
 
 export default LandingPage;
