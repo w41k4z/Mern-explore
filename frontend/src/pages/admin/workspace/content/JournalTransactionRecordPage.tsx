faae3b9034d51da0b77483475f7c5fd7334a1f81
w41k4z <alainricor@gmail.com>
2023-05-16 20:27:24 +0300

    Ok

--- frontend/src/pages/admin/workspace/content/JournalTransactionRecordPage.tsx
+++ frontend/src/pages/admin/workspace/content/JournalTransactionRecordPage.tsx
@@ -10,6 +10,7 @@ import { Society } from "../../../../models/society";
 import { UserAccount } from "../../../../models/userAccount";
 import { BsDownload, BsUpload } from "react-icons/bs";
 import { BiPlus } from "react-icons/bi";
+import { Modal } from "react-bootstrap";
 
 interface JournalTransactionRecordProps {
   society: Society;
@@ -30,8 +31,12 @@ const JournalTransactionRecordPage = ({
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [society._id]);
+  const [addModalVisibility, setAddModalVisibility] = useState(false);
 
   /* LOGIC */
+  const showAddModal = () => setAddModalVisibility(true);
+  const hideAddModal = () => setAddModalVisibility(false);
+
   const fetchAllCode = async () => {
     const formData = new FormData();
     formData.append("societyID", society._id);
@@ -44,6 +49,101 @@ const JournalTransactionRecordPage = ({
         alert(error);
       });
   };
+
+  /* ELEMENT */
+  const addModal = (
+    <Modal show fullscreen onHide={hideAddModal}>
+      <Modal.Header closeButton className="bg-dark text-white">
+        Insert new journal
+      </Modal.Header>
+      <Modal.Body>
+        <table className="table table-striped table-responsive">
+          <thead className="px-2 table-bordered">
+            <tr>
+              <th scope="col">Date</th>
+              <th scope="col">Part reference</th>
+              <th scope="col">Label</th>
+              <th scope="col">General account</th>
+              <th scope="col">Entitled</th>
+              <th scope="col">Third party</th>
+              <th scope="col">Debit</th>
+              <th scope="col">Credit</th>
+            </tr>
+          </thead>
+          <tbody className="px-2">
+            <tr>
+              <td>
+                <input className="form-control-sm" type="date" name="" id="" />
+              </td>
+              <td className="d-flex">
+                <select className="form-select-sm" name="" id="">
+                  <option value="">AN</option>
+                  <option value="">VE</option>
+                  <option value="">BO</option>
+                </select>
+                <input className="form-control-sm" type="text" />
+              </td>
+              <td>
+                <input className="form-control-sm" type="text" />
+              </td>
+              <td>
+                <input
+                  className="form-control-sm"
+                  type="datalist"
+                  id="generalChartOfAccount"
+                />
+                <datalist>
+                  <option value="">10000</option>
+                  <option value="">11000</option>
+                  <option value="">12800</option>
+                  <option value="">20100</option>
+                </datalist>
+              </td>
+              <td>
+                <input className="form-control-sm" type="text" />
+              </td>
+              <td>
+                <input
+                  className="form-control-sm"
+                  type="datalist"
+                  id="thirdPartyChartOfAccount"
+                />
+                <datalist>
+                  <option value="">Noro</option>
+                  <option value="">Solo</option>
+                  <option value="">Papa</option>
+                </datalist>
+              </td>
+              <td>
+                <input
+                  className="form-control-sm"
+                  type="number"
+                  name=""
+                  id=""
+                />
+              </td>
+              <td>
+                <input
+                  className="form-control-sm"
+                  type="number"
+                  name=""
+                  id=""
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </Modal.Body>
+      <Modal.Footer className="d-flex justify-content-end">
+        <div className="btn-group">
+          <button className="btn btn-secondary" onClick={hideAddModal}>
+            Cancel
+          </button>
+          <button className="btn btn-success">Add</button>
+        </div>
+      </Modal.Footer>
+    </Modal>
+  );
 
   return (
     <div className="m-4 bg-white">
@@ -90,9 +190,13 @@ const JournalTransactionRecordPage = ({
             <BsDownload style={{ fontSize: "20px" }} className="me-2" /> Export
             pdf
           </button>
-          <button className="ms-2 btn btn-outline-primary">
+          <button
+            className="ms-2 btn btn-outline-primary"
+            onClick={showAddModal}
+          >
             <BiPlus style={{ fontSize: "20px" }} />
           </button>
+          {addModalVisibility && addModal}
         </div>
       </div>
     </div>
